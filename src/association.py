16e1256fee90acdbff4267e1d6995df1600f3a04
aligned64 <113694089+aligned64@users.noreply.github.com>
2024-06-14 14:23:51 +0300

    added hot_encoding

--- src/association.py
+++ src/association.py
@@ -1,9 +1,27 @@
-from dataclasses import dataclass
 from pandas import DataFrame
 
-@dataclass
-class data:
-    zone_number: int
-    zone_letter: str
+def hot_encoding(earthquakes: DataFrame, volcanic_eruptions: DataFrame):
+    df = DataFrame({'Zone number': [], 'Zone letter': [], 'Earthquake': [], 
+                    'Volcanic eruption': []})
+    
+    def contains_location(zone_number: int, zone_letter: str) -> bool:
+        return ((df['Zone number'] == zone_number) & 
+                (df['Zone letter'] == zone_letter)).any()
+
+    for zone_number, zone_letter in zip(earthquakes['Zone number'], 
+                                        earthquakes['Zone letter']):
+
+        if not contains_location(zone_number, zone_letter):
+            df = df._append({'Zone number': zone_number, 'Zone letter': zone_letter, 'Earthquake': 1, 
+                    'Volcanic eruption': 0}, ignore_index=True)
+    
+    for zone_number, zone_letter in zip(volcanic_eruptions['Zone number'], 
+                                        volcanic_eruptions['Zone letter']): 
+        if not contains_location(zone_number, zone_letter):
+            df = df._append({'Zone number': zone_number, 'Zone letter': zone_letter, 'Earthquake': 0, 
+                    'Volcanic eruption': 1}, ignore_index=True)
+        else:
+            df.at[df.loc[(df['Zone number'] == zone_number) & (df['Zone letter'] == zone_letter)].index[0], 'Volcanic eruption'] = 1
+    return df
 
 # TODO: use mlxtend 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import dataset
+import association
 
 def main():
     earthquake_events = dataset.load_earthquake_events()
     volcano_events = dataset.load_volcano_events()
-    print(earthquake_events)
-    print(volcano_events)
+    # print(earthquake_events)
+    # print(volcano_events)
+    df = association.hot_encoding(earthquake_events, volcano_events)
+    print(df.to_string())
 
 if __name__ == "__main__":
     main()
