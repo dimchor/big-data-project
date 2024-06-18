3de484d6ee1a2e26384e6b4a6b87744a41b3f92b
Andrewmgk <151464838+Andrewmgk@users.noreply.github.com>
2024-06-18 20:10:08 +0300

    text

--- src/association.py
+++ src/association.py
@@ -5,6 +5,7 @@ def hot_encoding(earthquakes: DataFrame, volcanic_eruptions: DataFrame):
     df = DataFrame({'Zone number': [], 'Zone letter': [], 'Earthquake': [], 
                     'Volcanic eruption': []})
     
+    #returns true if there is a row which is equal to both zone_number and zone_letter
     def contains_location(zone_number: int, zone_letter: str) -> bool:
         return ((df['Zone number'] == zone_number) & 
                 (df['Zone letter'] == zone_letter)).any()
@@ -16,6 +17,7 @@ def hot_encoding(earthquakes: DataFrame, volcanic_eruptions: DataFrame):
             df = df._append({'Zone number': zone_number, 'Zone letter': zone_letter, 'Earthquake': 1, 
                     'Volcanic eruption': 0}, ignore_index=1)
     
+    #returns true if there is a row which is equal to both zone_number and zone_letter
     for zone_number, zone_letter in zip(volcanic_eruptions['Zone number'], 
                                         volcanic_eruptions['Zone letter']): 
         if not contains_location(zone_number, zone_letter):
